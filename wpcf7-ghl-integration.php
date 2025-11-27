91e1c9fff5ab9e783680caa899c6477d82e35ed8
Robert Ochoa <ochoa.robert1@gmail.com>
2025-11-26 22:20:04 -0400

    First Commit

---
+++ wpcf7-ghl-integration.php
@@ -0,0 +1,24 @@
+/**
+* Contact Form 7 GHL Integration
+*
+* @package ContactForm7GHLIntegration
+* @author Robert Ochoa
+* @copyright 2025 Robert Ochoa
+* @license GPL-2.0-or-later
+*
+* @wordpress-plugin
+* Plugin Name: Contact Form 7 GHL Integration
+* Plugin URI: https://robertochoaweb.com/casos/wpcf7-ghl-integration/
+* Description: Integrates GoHighLevel API to Contact Forms.
+* Version: 1.0.0
+* Requires at least: 6.0
+* Requires PHP: 8.0
+* Author: Robert Ochoa
+* Author URI: https://robertochoaweb.com/
+* Text Domain: wpcf7-ghl-integration
+* Domain Path: /languages
+* License: GPL v2 or later
+* License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+* Requires Plugins: contact-form-7
+*/
+
